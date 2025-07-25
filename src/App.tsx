79ae7043311d59eea83d561925fe1ad0dd427e30
Rodrigo Lima <Rodrigo.lima546@hotmail.com>
2025-07-25 01:47:28 -0300

    criado componente container e logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 
-import './App.css'
+import { Container} from "./components/Container";
+import { Heading } from "./components/Heading";
+import {Logo} from "./components/Logo";
+
+import '/styles/theme.css'
+import '/styles/global.css'
 
 export function App() {
   return (
-    <>
-    </>
+      <>
+        <Container>
+            <Logo>Logo</Logo>
+        </Container>
+
+        <Container>
+            <Heading>Menu</Heading>
+        </Container>
+      </>
   )
 }
