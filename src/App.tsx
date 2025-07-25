a9e3a8f5a310834cb2f7566662c5b6c7b217890b
Rodrigo Lima <Rodrigo.lima546@hotmail.com>
2025-07-25 13:00:31 -0300

    Menu done, started input component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
 import { Container} from "./components/Container";
-import { Heading } from "./components/Heading";
 import {Logo} from "./components/Logo";
+import {Menu} from "./components/Menu";
 
 import '/styles/theme.css'
 import '/styles/global.css'
+import {Countdown} from "./components/Countdown";
+import {DefaultInput} from "./components/DefaultInput";
 
 export function App() {
   return (
@@ -14,8 +16,34 @@ export function App() {
         </Container>
 
         <Container>
-            <Heading>Menu</Heading>
+            <Menu/>
         </Container>
+
+          <Container>
+              <Countdown/>
+          </Container>
+
+          <Container>
+              <form className='form' action=''>
+                  <div className='formRow'>
+                      <DefaultInput id='meuInput' type='text' />
+                  </div>
+
+                  <div className='formRow'>
+                      <p>Lorem ipsum dolor sit amet</p>
+                  </div>
+
+                 <div className='formRow'>
+                    <p>Ciclos</p>
+                    <p>0 0 0 0 0 0 0 0 0 0 </p>
+                 </div>
+
+                  <div className='formRow'>
+                    <button>Enviar</button>
+                  </div>
+              </form>
+          </Container>
+
       </>
   )
 }
